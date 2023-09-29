1c169650aaf8c8c181c2691fc63fd882144a4338
vikas-11 <samrathyadav1103@gmail.com>
2023-09-29 19:26:48 +0530

    Colors File

--- src/app/utils/colors.js
+++ src/app/utils/colors.js
@@ -1,1 +1,27 @@
- /*    */
+import { createTheme } from "@mui/material/styles";
+
+const Colors = createTheme({
+  palette: {
+
+    primary: {
+      main: "#FFFFFF",
+      darkBlue:"#002C4F",
+      lightBlue:"#007DE1",
+      red:"#DF1E23",
+    },
+
+    secondary:{
+      darkBlue:"#013C6B",
+      blue:"#007DE1",
+      lightBlue:"#008EFF",
+      brown:"#353535",
+      // red:"#DF1E23",
+      lightGreen:"#00C1B6",
+      yellow:"#F0CA00",
+      purple:"#DC77FF",
+      darkPurple:"#A300F0" ,  
+    }
+  },
+});
+
+export default Colors;
