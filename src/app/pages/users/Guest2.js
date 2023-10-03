cd71b128391ba7b9ae41ce844184425aa37c13ea
Nikita Mer <mernikita127@gmail.com>
2023-10-03 22:30:42 +0530

    Subscribe.js
    Subscribe page

--- src/app/pages/users/Guest2.js
+++ src/app/pages/users/Guest2.js
@@ -3,6 +3,7 @@ import Nav from '../../partials/Nav';
 import Ourplans from './Ourplans';
 import Footer from '../../partials/Footer';
 import Cards from './Cards';
+import Subscribe from './Subscribe';
 
 
 const Guest2 = () => {
@@ -11,6 +12,7 @@ const Guest2 = () => {
       <Nav/>
       <Ourplans/>
     <Cards/>
+    <Subscribe/>
       <Footer/> 
     </>
   );
