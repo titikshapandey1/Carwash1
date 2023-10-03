b236570eaebb253780a4d20282323a06782fbfb5
Vikas Yadav <samrathyadav1103@gmail.com>
2023-10-03 17:44:24 +0530

    ContactUs

--- src/App.js
+++ src/App.js
@@ -1,61 +1,58 @@
-
-// import Login from "../src/app/pages/Login"
-// import Guest1 from './app/components/Background';
-// import HomePage from './app/components/Home/Homepage';
-// import PaymentOptions from './app/components/Paymentoption';
-// import AboutGuest from './app/pages/users/About';
-// import ServiceLocation from './app/pages/users/Service';
-// import Nav from "./app/partials/Nav";
-// import Question from "./app/components/Question";
-// import Footer from "./app/partials/Footer";
-// import RegisterPage from "./app/pages/users/Register";
+import Login from "../src/app/pages/Login"
+import Guest1 from './app/components/Background';
+import HomePage from './app/components/Home/Homepage';
+import PaymentOptions from './app/components/Paymentoption';
+import AboutGuest from './app/pages/users/About';
+import ServiceLocation from './app/pages/users/Service';
+import Nav from "./app/partials/Nav";
+import Question from "./app/components/Question";
+import Footer from "./app/partials/Footer";
+import RegisterPage from "./app/pages/users/Register";
 import Mobile from "./app/pages/users/Mobile";
-// import Rectanglefield from "./app/components/Rectanglefield";
-// import Otp from "./app/pages/users/otp";
-// import Ourplan from "./app/pages/users/Ourplan";
-// import Subscriptions from "./app/pages/users/Subscription";
-// import Booking from "./app/pages/users/Booking";
+import Rectanglefield from "./app/components/Rectanglefield";
+import Otp from "./app/pages/users/otp";
+import Ourplan from "./app/pages/users/Ourplan";
+import Subscriptions from "./app/pages/users/Subscription";
+import Booking from "./app/pages/users/Booking";
 import OurServices from "./app/components/OurServices";
 import Card1 from "./app/components/Card";
 import Guest5 from "./app/pages/users/Guest5";
 import Guest2 from "./app/pages/users/Guest2";
 import Ourplans from "./app/pages/users/Ourplans";
-// import Contactus from "./app/pages/users/Contactus";
-// import Dryclean from "./app/components/Home/Dryclean";
-
-// import HomeCards from "./app/components/Home/HomeCards";
-
+import Contactus from "./app/pages/users/Contactus";
+import Dryclean from "./app/components/Home/Dryclean";
+import HomeCards from "./app/components/Home/HomeCards";
 
 // {hii}
 
 function App() {
   return (
-   <>
-   {/* <Dryclean/> */}
-   {/* <Card1/> */}
-   {/* <HomeCards/> */}
-   {/* <PaymentOptions/> */}
-   {/* <Ourplan/> */}
-   {/* <Nav/>  */}
-   {/* <Guest5/> */}
-    {/* <HomePage/> */}
-   {/* <ServiceLocation/> */}
-    {/* <AboutGuest/> */}
-   {/* <Guest1/>   */}
- {/* <Login/> */}
- {/* <Question/> */}
- {/* <RegisterPage/> */}
- {/* <Mobile/> */}
- {/* <Otp/> */}
- {/* <Rectanglefield/> */}
- {/* <Subscriptions/> */}
- {/* <OurServices/> */}
- {/* <Booking/> */}
-  {/* <Footer/> */}
-  {/* <Contactus/> */}
-  <Guest2/>
-  {/* <Ourplans/> */}
-   </>
+    <>
+      {/* <Dryclean/> */}
+      {/* <Card1/> */}
+      {/* <HomeCards/> */}
+      {/* <PaymentOptions/> */}
+      {/* <Ourplan/> */}
+      {/* <Nav/>  */}
+      {/* <Guest5/> */}
+      {/* <HomePage/> */}
+      {/* <ServiceLocation/> */}
+      {/* <AboutGuest/> */}
+      {/* <Guest1/>   */}
+      {/* <Login/> */}
+      {/* <Question/> */}
+      {/* <RegisterPage/> */}
+      {/* <Mobile/> */}
+      {/* <Otp/> */}
+      {/* <Rectanglefield/> */}
+      {/* <Subscriptions/> */}
+      {/* <OurServices/> */}
+      {/* <Booking/> */}
+      {/* <Footer/> */}
+      <Contactus/>
+      {/* <Guest2 /> */}
+      {/* <Ourplans/> */}
+    </>
   );
 }
 
