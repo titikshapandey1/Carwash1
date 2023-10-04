e840e334d4d0839e97560220b37e849c79c5ca21
Nikita Mer <mernikita127@gmail.com>
2023-10-04 10:59:19 +0530

    searcher

--- src/app/pages/users/Searcher.js
+++ src/app/pages/users/Searcher.js
@@ -1,13 +1,14 @@
 import { Box, Button } from '@mui/material';
 import React from 'react';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Colors from '../../utils/colors';
 
 const Searcher = () => {
   return (
     <>
-    {/* hii */}
-       <Box sx={{background: "#013C6B"}}>
-       <Button variant="Text" sx={{ backgroundColor: "white", color: "black",borderRadius:'10px' , margin: "2%"}}>
+  {/* hii */}
+       <Box sx={{background: Colors.palette.secondary.main}}>
+       <Button variant="Text" sx={{ background: Colors.palette.primary.main, color: Colors.palette.secondary.black,borderRadius:'10px' , margin: "2%"}}>
               Select Service Type   <ArrowDropDownIcon/>
             </Button>
 
