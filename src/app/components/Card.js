df1a3a2cc2525a256c2527fd1a542f02b855630a
Vikas Yadav <samrathyadav1103@gmail.com>
2023-10-05 18:21:28 +0530

    Our Services

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,187 +1,265 @@
-import { Box, Button, Card, CardContent, Container, Grid, Typography } from '@mui/material';
-import React from 'react';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import OurServices from '../components/OurServices';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import OurServices from "../components/OurServices";
 import Colors from "../utils/colors";
 import { NavLink } from "react-router-dom";
 
 const Card1 = () => {
   return (
-  <>
- 
-    <OurServices/>
-    <Container sx={{ marginTop: "3%" }}>
-      <Grid container item spacing={2} justifyContent="center">
-        <Grid item xs={12} sm={6} md={4}>
-          <Card sx={{ borderRadius: "20px", bgcolor: Colors.palette.primary.main }}>
-            <CardContent>
-              <Typography variant='h5' sx={{ marginTop: "5%", textAlign: "center", color:Colors.palette.secondary.main }}>
-                <b> Routine Clean<br />₹  199</b>
-              </Typography>
-              <Typography>
-                <ul>
-                  <li>
-                  Exterior Wash, removing dirt, dust, and grime.
-
-                  </li> <br />
-                  <li>
-                    Interior Vacuuming, dusting seats and carpets and floor mats.
-                  </li> <br />
-                  <li>
-                    Cleaning and Polishing all windows and mirrors.
-                  </li> <br />
-                  <li>
-                    Cleaning and Shining tires and rims.
-                  </li> <br />
-                  <li>
-                    Wiping down interior surface, including dashboard and door panels.
-                  </li>
-                </ul>
-              </Typography>
-
-              <Typography>
-                <Button variant='contained'
+    <>
+      <OurServices />
+      <Container sx={{ marginTop: "3%" }}>
+        <Grid container item spacing={2} justifyContent="center">
+          <Grid item xs={12} sm={6} md={4}>
+            <Card
+              sx={{
+                borderRadius: "20px",
+                bgcolor: Colors.palette.primary.main,
+              }}
+            >
+              <CardContent>
+                <Typography
+                  variant="h5"
                   sx={{
-                    borderRadius: "22px",
-                    bgcolor: Colors.palette.primary.darkBlue,
-                    height: "55px",
-                    width: "150px",
-                    marginInline: "30%",
+                    marginTop: "5%",
+                    textAlign: "center",
+                    color: Colors.palette.secondary.main,
                   }}
                 >
-                  <NavLink
-                    to="/services"
-                    style={{
-                      textDecoration: "none",
-                      color: Colors.palette.primary.main,
-                      display: "flex",
-                      alignItems: "center",
-                    }}
-                  >
-                    Book now   <ArrowForwardIosIcon sx={{ fontSize: "medium", marginTop: "-2px" }} />
-                  </NavLink>
-                  
-                </Button>
-              </Typography>
+                  <b>
+                    {" "}
+                    Routine Clean
+                    <br />₹ 199
+                  </b>
+                </Typography>
+                <Typography>
+                  <ul>
+                    <li>Exterior Wash, removing dirt, dust, and grime.</li>{" "}
+                    <br />
+                    <li>
+                      Interior Vacuuming, dusting seats and carpets and floor
+                      mats.
+                    </li>{" "}
+                    <br />
+                    <li>
+                      Cleaning and Polishing all windows and mirrors.
+                    </li>{" "}
+                    <br />
+                    <li>Cleaning and Shining tires and rims.</li> <br />
+                    <li>
+                      Wiping down interior surface, including dashboard and door
+                      panels.
+                    </li>
+                  </ul>
+                </Typography>
 
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} sm={6} md={4}>
-          <Card sx={{ borderRadius: "20px", bgcolor: Colors.palette.primary.main }}>
-            <CardContent>
-              <Typography variant='h5' sx={{ marginTop: "5%", textAlign: "center", color: Colors.palette.secondary.main }}>
-                <b> Dry Clean<br />₹ 799</b>
-              </Typography>
-              <Typography>
-                <ul>
-                  <li>
-                  Cleaning of the exterior removing dirt, dust, and grime.
-
-                  </li> <br />
-                  <li>
-                  Vacuuming, wiping, and sanitizing interior, including seats, dashboard, and windows.
-                  </li> <br />
-                  <li>
-                  Specialized cleaning of seats, carpets, and fabric surfaces to remove stains and odors.
-
-                  </li> <br />
-                  <li>
-                  Application of protective coatings or sealants to maintain the car's paint finish.
-
-                  </li> <br />
-                  <li>
-                  Fine detailing work, polishing, waxing, and tire cleaning.
-                  </li>
-                </ul>
-              </Typography>
-              <Typography>
-                <Button variant='contained'
+                <Box
                   sx={{
-                    borderRadius: "22px",
-                    bgcolor: Colors.palette.primary.darkBlue,
-                    height: "55px",
-                    width: "150px",
-                    marginInline: "30%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginTop: "5%",
                   }}
                 >
-                  <NavLink
-                    to="/services"
-                    style={{
-                      textDecoration: "none",
-                      color: Colors.palette.primary.main,
-                      display: "flex",
-                      alignItems: "center",
+                  <Button
+                    variant="contained"
+                    sx={{
+                      borderRadius: "22px",
+                      background: Colors.palette.secondary.main,
+                      height: "55px",
+                      width: "150px",
                     }}
                   >
-                    Book now   <ArrowForwardIosIcon sx={{ fontSize: "medium", marginTop: "-2px" }} />
-                  </NavLink>
-                </Button>
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+                    <NavLink
+                      to="/services"
+                      style={{
+                        textDecoration: "none",
+                        color: Colors.palette.primary.main,
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                    >
+                      Book now &nbsp;
+                      <ArrowForwardIosIcon sx={{ fontSize: "medium" }} />
+                    </NavLink>
+                  </Button>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
 
-        <Grid item xs={12} sm={6} md={4}>
-          <Card sx={{ borderRadius: "20px", bgcolor: Colors.palette.primary.main }}>
-            <CardContent>
-              <Typography variant='h5' sx={{ marginTop: "5%", textAlign: "center", color: Colors.palette.secondary.main }}>
-                <b> Deep Clean<br />₹ 999</b>
-              </Typography>
-              <Typography>
-                <ul>
-                  <li>
-                  Thorough cleaning of seats, carpets, and upholstery
-                  </li> <br />
-                  <li>
-                 Cleaning, waxing, and polishing the car's exterior.
-
-                  </li> <br />
-                  <li>
-                  Removing dirt and grime from the engine compartment.
-
-                  </li> <br />
-                  <li>
-                  Detailed cleaning and tire dressing
-
-                  </li> <br />
-                  <li>
-                  Ensuring windows and mirrors are streak-free for visibility and safety.
-                  </li>
-                </ul>
-              </Typography>
-
-              <Typography>
-                <Button variant='contained'
+          <Grid item xs={12} sm={6} md={4}>
+            <Card
+              sx={{
+                borderRadius: "20px",
+                bgcolor: Colors.palette.primary.main,
+              }}
+            >
+              <CardContent>
+                <Typography
+                  variant="h5"
                   sx={{
-                    borderRadius: "22px",
-                    bgcolor: Colors.palette.primary.darkBlue,
-                    height: "55px",
-                    width: "150px",
-                    marginInline: "30%",
+                    marginTop: "5%",
+                    textAlign: "center",
+                    color: Colors.palette.secondary.main,
                   }}
                 >
-                  <NavLink
-                    to="/services"
-                    style={{
-                      textDecoration: "none",
-                      color: Colors.palette.primary.main,
-                      display: "flex",
-                      alignItems: "center",
+                  <b>
+                    {" "}
+                    Dry Clean
+                    <br />₹ 799
+                  </b>
+                </Typography>
+                <Typography>
+                  <ul>
+                    <li>
+                      Cleaning of the exterior removing dirt, dust, and grime.
+                    </li>{" "}
+                    <br />
+                    <li>
+                      Vacuuming, wiping, and sanitizing interior, including
+                      seats, dashboard, and windows.
+                    </li>{" "}
+                    <br />
+                    <li>
+                      Specialized cleaning of seats, carpets, and fabric
+                      surfaces to remove stains and odors.
+                    </li>{" "}
+                    <br />
+                    <li>
+                      Application of protective coatings or sealants to maintain
+                      the car's paint finish.
+                    </li>{" "}
+                    <br />
+                    <li>
+                      Fine detailing work, polishing, waxing, and tire cleaning.
+                    </li>
+                  </ul>
+                </Typography>
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginTop: "5%",
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    sx={{
+                      borderRadius: "22px",
+                      background: Colors.palette.secondary.main,
+                      height: "55px",
+                      width: "150px",
                     }}
                   >
-                    Book now   <ArrowForwardIosIcon sx={{ fontSize: "medium", marginTop: "-2px" }} />
-                  </NavLink>
-                </Button>
-              </Typography>
-            </CardContent>
-          </Card>
+                    <NavLink
+                      to="/services"
+                      style={{
+                        textDecoration: "none",
+                        color: Colors.palette.primary.main,
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                    >
+                      Book now &nbsp;
+                      <ArrowForwardIosIcon sx={{ fontSize: "medium" }} />
+                    </NavLink>
+                  </Button>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Card
+              sx={{
+                borderRadius: "20px",
+                bgcolor: Colors.palette.primary.main,
+              }}
+            >
+              <CardContent>
+                <Typography
+                  variant="h5"
+                  sx={{
+                    marginTop: "5%",
+                    textAlign: "center",
+                    color: Colors.palette.secondary.main,
+                  }}
+                >
+                  <b>
+                    {" "}
+                    Deep Clean
+                    <br />₹ 999
+                  </b>
+                </Typography>
+                <Typography>
+                  <ul>
+                    <li>Thorough cleaning of seats, carpets, and upholstery</li>{" "}
+                    <br />
+                    <li>
+                      Cleaning, waxing, and polishing the car's exterior.
+                    </li>{" "}
+                    <br />
+                    <li>
+                      Removing dirt and grime from the engine compartment.
+                    </li>{" "}
+                    <br />
+                    <li>Detailed cleaning and tire dressing</li> <br />
+                    <li>
+                      Ensuring windows and mirrors are streak-free for
+                      visibility and safety.
+                    </li>
+                  </ul>
+                </Typography>
+
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginTop: "5%",
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    sx={{
+                      borderRadius: "22px",
+                      background: Colors.palette.secondary.main,
+                      height: "55px",
+                      width: "150px",
+                    }}
+                  >
+                    <NavLink
+                      to="/services"
+                      style={{
+                        textDecoration: "none",
+                        color: Colors.palette.primary.main,
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                    >
+                      Book now &nbsp;
+                      <ArrowForwardIosIcon sx={{ fontSize: "medium" }} />
+                    </NavLink>
+                  </Button>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
     </>
   );
-}
+};
 
 export default Card1;
--- src/app/components/OurServices.js
+++ src/app/components/OurServices.js
@@ -2,22 +2,55 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import { Container, Card } from "@mui/material";
+import { Container, Card, Box } from "@mui/material";
 import red from "../assests/images/red.png";
 import yellow from "../assests/images/yellow.png";
 import blue from "../assests/images/blue.png";
 import Colors from "../utils/colors";
+import Divider from "@mui/material/Divider";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { Link } from "react-router-dom";
 
 const OurServices = () => {
   return (
     <Container sx={{ marginTop: "3%" }}>
+      <Box sx={{ display: "flex", alignItems: "center",marginBottom:"3%" }}>
+        <Typography
+          variant="h5"
+          sx={{ color: Colors.palette.secondary.main, fontWeight: "600" }}
+        >
+          Our Services&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;•
+        </Typography>
+        <Divider
+          sx={{
+            backgroundColor: Colors.palette.secondary.main,
+            height: "2px",
+            flex: 1,
+            marginLeft: "2%",
+          }}
+        />
+        <Typography
+          sx={{
+            color: Colors.palette.secondary.main,
+            display: "flex",
+            alignItems: "center",
+            marginLeft: "1.5%",
+          }}
+        >
+          see all <ArrowForwardIosIcon sx={{ fontSize: "12px" }} />
+        </Typography>
+      </Box>
+
       <Grid container spacing={4}>
         <Grid item xs={12} sm={4}>
-          <Card>
+          <Card
+            sx={{ background: Colors.palette.primary.main, boxShadow: "none" }}
+          >
             <Typography
               variant="h5"
               sx={{
-                backgroundColor: Colors.palette.background.default,
+                backgroundColor: Colors.palette.secondary.main,
+                color: Colors.palette.primary.main,
                 textAlign: "center",
               }}
             >
@@ -32,8 +65,15 @@ const OurServices = () => {
         </Grid>
 
         <Grid item xs={12} sm={4}>
-          <Card>
-            <Typography variant="h5" sx={{ textAlign: "center" }}>
+          <Card sx={{ boxShadow: "none" }}>
+            <Typography
+              variant="h5"
+              sx={{
+                textAlign: "center",
+                color: Colors.palette.secondary.main,
+                fontWeight: "600",
+              }}
+            >
               Sedan
             </Typography>
             <img
@@ -45,8 +85,15 @@ const OurServices = () => {
         </Grid>
 
         <Grid item xs={12} sm={4}>
-          <Card>
-            <Typography variant="h5" sx={{ textAlign: "center" }}>
+          <Card sx={{ boxShadow: "none" }}>
+            <Typography
+              variant="h5"
+              sx={{
+                textAlign: "center",
+                color: Colors.palette.secondary.main,
+                fontWeight: "600",
+              }}
+            >
               SUV
             </Typography>
             <img
