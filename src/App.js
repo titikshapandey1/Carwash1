62b578306875d109028b4647b37b193abdf58679
Titiksha2 <titikshapandey1@gmail.com>
2023-10-17 14:04:08 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
+import Mobile from "./app/pages/users/auth/Mobile";
 import Index from "./app/routes/index";
 
 function App() {
   return (
     <>
-   
+   {/* <Mobile/> */}
       <Index />
     </>
   );
--- src/app/pages/users/auth/Mobile.js
+++ src/app/pages/users/auth/Mobile.js
@@ -194,3 +194,5 @@ const Mobile = () => {
 };
 
 export default Mobile;
+
+
