e66e305dac3bd4446f2e831907378b35b91151a9
Vikas Yadav <samrathyadav1103@gmail.com>
2023-10-27 14:03:45 +0530

    corrections

--- src/app/components/Searcher.js
+++ src/app/components/Searcher.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { TextField, Autocomplete, Button, Box, Grid} from "@mui/material";
+import { TextField, Autocomplete, Button, Box, Grid } from "@mui/material";
 import Colors from "../utils/colors";
 
 const Searcher = () => {
   const [inputValue1, setInputValue1] = useState("");
   const [inputValue2, setInputValue2] = useState("");
-  const [options, 
-  ] = useState([
+  const [options] = useState([
     "4Wheeler",
     "Audi",
     "BMW",
@@ -22,11 +21,15 @@ const Searcher = () => {
     setInputValue2(newInputValue);
   };
 
-
   return (
     <Box sx={{ background: Colors.palette.secondary.main }}>
-      <Grid container spacing={1} >
-        <Grid item xs={12} sm={4}  sx={{marginLeft:'5px', marginRight:'5px'}} >
+      <Grid container spacing={1}>
+        <Grid
+          item
+          xs={12}
+          sm={4}
+          sx={{ marginLeft: "5px", marginRight: "5px" }}
+        >
           <Autocomplete
             value={inputValue1}
             onChange={(event, newValue) => {
@@ -43,15 +46,19 @@ const Searcher = () => {
                 fullWidth
                 sx={{
                   backgroundColor: "white",
-                  borderRadius: "5px",               
-                  marginBottom: "2%", 
-                 
+                  borderRadius: "5px",
+                  marginBottom: "2%",
                 }}
               />
             )}
           />
         </Grid>
-        <Grid item xs={12} sm={4} sx={{marginLeft:'5px', marginRight:'5px'}}>
+        <Grid
+          item
+          xs={12}
+          sm={4}
+          sx={{ marginLeft: "5px", marginRight: "5px" }}
+        >
           <Autocomplete
             value={inputValue2}
             onChange={(event, newValue) => {
@@ -70,26 +77,26 @@ const Searcher = () => {
                   backgroundColor: "white",
                   borderRadius: "5px",
                   marginBottom: "2%",
-             
                 }}
               />
             )}
           />
         </Grid>
-        <Grid item sx={{ display: "flex", justifyContent: "center" ,marginLeft:'5px'}} >
+        <Grid
+          item
+          sx={{ display: "flex", justifyContent: "center", marginLeft: "5px" }}
+        >
           <Button
             variant="Text"
             sx={{
-              backgroundColor: Colors.palette.primary.main,
-              color: Colors.palette.secondary.black,
+              width: "100px",
               borderRadius: "10px",
               marginBottom: "10%",
-             
-
+              backgroundColor: Colors.palette.primary.main,
+              color: Colors.palette.secondary.black,
               "&:hover": {
                 backgroundColor: Colors.palette.primary.lightBlue,
                 color: Colors.palette.primary.main,
-                border: `1px solid ${Colors.palette.primary.main}`,
               },
             }}
           >
