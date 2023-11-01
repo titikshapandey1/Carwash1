1241d110785b93b6b032f824f513f8961a84f3dc
Titiksha2 <titikshapandey1@gmail.com>
2023-11-01 14:41:56 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import AdEditSup1 from "./app/pages/admin/AdEditSup1";
-import RegisterPage from "./app/pages/users/auth/Register";
 import Index from "./app/routes/index";
 function App() {
   return (
     <>
-      {/* <Index /> */}
-      <AdEditSup1/>
+      <Index />
     </>
   );
 }
